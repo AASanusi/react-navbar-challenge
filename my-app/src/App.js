be0830e6c76e8c05862f1c6168124a4d425471d1
AASanusi <alaminm.sanusi@gmail.com>
2023-02-06 21:48:52 +0000

    Add my app and navbar

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBarSimple from './components/NavBarSimple';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBarSimple name="AYPHEN" />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/NavBarSimple.js
@@ -0,0 +1,12 @@
+import REACT from "react";
+
+function NavBarSimple (props) {
+    return (
+        <h1>My Gallery</h1>
+
+    )
+    
+
+}
+
+export default NavBarSimple;
